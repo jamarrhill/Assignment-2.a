763d526c4b91f2e34fcf21758bb841e67b48f2f5
jamarrhill <hilljama@oregonstate.edu>
2020-10-07 14:54:13 -0800

    Assignment 2.b

---
+++ temp_convert.py
@@ -0,0 +1,8 @@
+# Name: Jamar Hill
+# Date: 10/07/20
+# Description: Temperature Conversion
+
+print("Please enter a Celsius temperature")
+C = float(input())
+print("The equivalent Fahrenheit temperature is:")
+print((9/5) * C + 32)
