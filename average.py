a7b48c4a7b5bb91d4b35a1c3782119269a6c29b7
jamarrhill <hilljama@oregonstate.edu>
2020-10-07 14:45:21 -0800

    Assignment 2.a

---
+++ average.py
@@ -0,0 +1,19 @@
+@@ -0,0 +1,18 @@
+# Jamar Hill
+# 10/07/2020
+# averge.py
+
+print("Please enter five numbers")
+print("First Number")
+input_1 = float(input())
+print("Second Number")
+input_2 = float(input())
+print("Third Number")
+input_3 = float(input())
+print("Fourth Number")
+input_4 = float(input())
+print("Fifth Number")
+input_5 = float(input())
+solution = (input_1 + input_2 + input_3 + input_4 + input_5)/5
+print("The average of these number is:")
+print(solution)
